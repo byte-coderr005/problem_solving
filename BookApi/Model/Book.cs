48ffdf72ad7f7272b035c0bd07cbd94b54e7a826
Burak Ozturk <119581350+byte-coderr005@users.noreply.github.com>
2025-08-06 09:20:34 +0300

    Delete BookApi directory

--- BookApi/Model/Book.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BookApi.Model
-{
-    public class Book
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Author { get; set; }
-    }
-}
